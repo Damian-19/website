c673c3b064760e0266aea236b2e1288d08581605
Damian <18230253@studentmail.ul.ie>
2020-10-26 21:41:51 +0000

    Updated dark-mode.js

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,25 +1,27 @@
 	function darkMode() {
-		document.body.classList.toggle("dark-mode")
-		document.getElementById("topnav").classList.toggle("nav-dark-mode");
+		document.body.classList.toggle("dark-mode") // add dark-mode class to body
+		document.getElementById("topnav").classList.toggle("nav-dark-mode"); // add dark mode class to nav
 	}
 
 	function storeDarkMode() {
-		if (localStorage.getItem(1) == 1) {
-		localStorage.removeItem(1, 1);
-		} else if (localStorage.getItem(1) != 1) {
-			localStorage.setItem(1, 1);
+		if (localStorage.getItem(1) == 1) { // check if key pair exists
+		localStorage.removeItem(1, 1); // remove dark mode key pair
+		} else if (localStorage.getItem(1) != 1) { // check if key pair doesn't exist
+			localStorage.setItem(1, 1);	// add dark mode key pair
 		}
 	}
 
 	function setStoredDarkMode(id) {
 		var value = localStorage.getItem(id);
-		if(value != undefined) {
+		if(value != undefined) { // check key pair exists
 			document.getElementById("topnav").classList.add("nav-dark-mode");
 			document.body.classList.add("dark-mode");
 		}
     }
 
-    /* Used to change dark mode image on switch, no longer in use
+/* 
+	**Used to change dark mode image on switch, no longer in use**
+	
 function changeImg() {
 	if (localStorage.getItem(1) == 1) {
 		document.getElementById("change-img").src = "icons/light.png";
